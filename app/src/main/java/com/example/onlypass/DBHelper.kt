cf9816a8eb39c08a995f452274bb25bf7a8d6ac3
Dayoming <jdayoung1204@naver.com>
2022-11-17 05:09:15 +0900

    DB 연동을 위한 DBHelper Class 추가

--- app/src/main/java/com/example/onlypass/DBHelper.kt
+++ app/src/main/java/com/example/onlypass/DBHelper.kt
@@ -1,4 +1,26 @@
 package com.example.onlypass
 
-class DBHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper (context: Context?,
+                name: String?, // 데이터 베이스 이름
+                factory: SQLiteDatabase.CursorFactory? = null, // null을 기본 값으로 함
+                version: Int // 데이터 베이스 버전
+) : SQLiteOpenHelper(context, name, factory, version) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        // 테이블 생성
+        val memoSQL = "create table password_info (" +
+                "_id integer primary key autoincrement," +
+                "web_name, password, web_address, category, date, memo)"
+        db?.execSQL(memoSQL)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        // 기존 테이블 제거후 다시 생성하기
+        db?.execSQL("drop table password_info")
+        onCreate(db)
+    }
 }
